52d3d03c687c45f85fcbd9d755d42aa4785f1422
Patilrupali2002 <117559281+Patilrupali2002@users.noreply.github.com>
2023-08-24 12:14:32 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,9 +1,9 @@
 class Simple
 {
-public static void main(string[]args){
+public static void main(String[]args){
 int a=2;
 int b=3;
 c=a+b;
-System.Out.println("Addition of two no",c)
+System.out.println("Addition of two no",c)
 }
 }
